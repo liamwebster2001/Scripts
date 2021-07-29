30582a407e940cd8abe64a2050298c147803a3b7
Liam Webster <liamwebster2001@berkeley.edu>
2021-07-29 15:22:39 -0700

    added a youtube audio downloader built via python

---
+++ ytConverter.py
@@ -0,0 +1,31 @@
+from youtube_dl import YoutubeDL
+
+import os
+
+
+
+os.chdir('Desktop/Dj/Music')
+
+audio_downloader = YoutubeDL({'format':'m4a'})
+
+while True:
+
+    try:
+
+        print('Youtube Downloader'.center(40, '_'))
+
+        URL = input('Enter youtube url :  ')
+
+        audio_downloader.extract_info(URL)
+
+    except Exception:
+
+        print("Couldn\'t download the audio")
+
+    finally:
+
+        option = int(input('\n1.download again \n2.Exit\n\nOption here :'))
+
+        if option!=1:
+
+            break
