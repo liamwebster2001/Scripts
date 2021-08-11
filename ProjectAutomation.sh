150428d8c2b0774eb7cbc20f1756855094ca2736
liamwebsterreal <liamwebster2001@berkeley.edu>
2021-08-11 14:37:59 -0700

    fixed api and tuned for windows

--- ProjectAutomation.sh
+++ ProjectAutomation.sh
@@ -2,6 +2,8 @@
 
 CURRENTDIR=${pwd}
 
+cd
+
 # step 1: name of the remote repo. Enter a SINGLE WORD ..or...separate with hyphens
 echo "What name do you want to give your remote repo?"
 read REPO_NAME
@@ -12,11 +14,11 @@ read DESCRIPTION
 echo "Enter a folder name: "
 read FOLDER_NAME
 
-PROJ_PATH="/Users/liamwebster/Documents/Projects"
+PROJ_PATH="/mnt/c/Users/liamc/Documents/Projects"
 cd "$PROJ_PATH"
 mkdir $FOLDER_NAME
 
-PROJECT_PATH="/Users/liamwebster/Documents/Projects/$FOLDER_NAME"
+PROJECT_PATH="/mnt/c/Users/liamc/Documents/Projects/$FOLDER_NAME"
 cd "$PROJECT_PATH"
 
 # step 4: initialise the repo locally, create blank README, add and commit
@@ -29,15 +31,15 @@ git commit -m 'initial commit -setup with .sh script'
 # step 5 use github API to log the user in
 #curl -u liamwebsterreal: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 -d "{\"name\": \"${REPO_NAME}\", \"description\": \"${DESCRIPTION}\"}" https://api.github.com/user/repos
 
-curl -H "Authorization: token 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" -d "{\"name\": \"${REPO_NAME}\", \"description\": \"${DESCRIPTION}\"}" https://api.github.com/user/repos
-
-
 #  step 6 add the remote github repo to local repo and push
-git remote add origin https://github.com/liamwebsterreal/${REPO_NAME}.git
-git push --set-upstream origin master
+git remote add origin git@github.com:liamwebsterreal/${REPO_NAME}.git
+git branch -M main
+git push -u origin main
+#@git remote add origin https://github.com/liamwebsterreal/${REPO_NAME}.git
+#git push --set-upstream origin master
 
 # step 7 change to your project's root directory.
 cd "$PROJECT_PATH"
 
-echo "Done. Go to https://github.com/liamwebsterreal/$REPO_NAME to see." 
+echo "Done. Go to https://github.com/$USERNAME/$REPO_NAME to see." 
 echo " *** You're now in your project root. ***"
