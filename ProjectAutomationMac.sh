9e6de41a2b1eb8409691e25a804732087e7af436
Liam Webster <liamwebster2001@berkeley.edu>
2021-11-10 21:17:07 -0800

    updated

--- ProjectAutomationMac.sh
+++ ProjectAutomationMac.sh
@@ -28,27 +28,23 @@ git add README.MD
 git commit -m 'initial commit -setup with .sh script'
 
 
-curl \
+curl --s \
   -X POST \
-  -H "Authorization: token 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" \
+  -H "Authorization: token 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" \
   https://api.github.com/user/repos \
   -d '{"name":"'"$REPO_NAME"'"}'
-
-# step 5 use github API to log the user in
-#curl -u liamwebsterreal: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 -d "{\"name\": \"${REPO_NAME}\", \"description\": \"${DESCRIPTION}\"}" https://api.github.com/user/repos
-#curl -H "Authorization: token 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" --data '{"name":'$REPO_NAME'}' https://api.github.com/user/repos
+  
 
 
 #  step 6 add the remote github repo to local repo and push
-git remote add origin git@github.com:liamwebsterreal/${REPO_NAME}.git
+git remote add origin git@github.com:liamwebsterreal/$REPO_NAME.git
 git branch -M main
 git push -u origin main
-#@git remote add origin https://github.com/liamwebsterreal/${REPO_NAME}.git
-#git push --set-upstream origin master
+
 
 # step 7 change to your project's root directory.
 cd "$PROJECT_PATH"
-code OUTLINE.txt
 
-echo "Done. Go to https://github.com/$USERNAME/$REPO_NAME to see." 
+
+echo "Done. Go to https://github.com/liamwebsterreal/$REPO_NAME to see." 
 echo " *** You're now in your project root. ***"
